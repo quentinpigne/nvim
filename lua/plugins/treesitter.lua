2feaecabf73ffa5d309dc37ff1257f2f5f9b63f2
Quentin Pigné <quentin.pigne@gmail.com>
2024-03-14 06:55:44 +0000

    Improving treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,5 +5,13 @@ return {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "css", "html", "javascript", "json", "lua", "markdown", "rust", "scss", "typescript", "vue", "yaml" },
+        auto_install = true,
+      })
+    end
   },
 }
